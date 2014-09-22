d051fb91b2b7ab7f27758de44f649287cc3f55e8
Ian Parker <ian@geekprojects.com>
2014-09-22 20:46:11 +0100

    Core/Random: Tidy up and add header

--- src/libgeek-core/random.cpp
+++ src/libgeek-core/random.cpp
@@ -1,3 +1,38 @@
+/*
+   Mersenne Twister PRNG
+
+   Based on code by Takuji Nishimura and Makoto Matsumoto.
+
+   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+     1. Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+     2. Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+
+     3. The names of its contributors may not be used to endorse or promote
+        products derived from this software without specific prior written
+        permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include <time.h>
 #include <stdio.h>
@@ -92,27 +127,27 @@ bool Random::init_urandom()
         return false;
     }
 
-while (n < MT_N)
-{
-uint32_t word;
-int res;
-res = fread(&word, 4, 1, fd);
-if (res != 1)
-{
-break;
-}
-    entropy[n++] = word;
-}
+    while (n < MT_N)
+    {
+        uint32_t word;
+        int res;
+        res = fread(&word, 4, 1, fd);
+        if (res != 1)
+        {
+            break;
+        }
+        entropy[n++] = word;
+    }
     fclose(fd);
 
-if (n < MT_N)
-{
-    entropy[n++] = time(NULL);
-}
-if (n < MT_N)
-{
-    entropy[n++] = clock();
-}
+    if (n < MT_N)
+    {
+        entropy[n++] = time(NULL);
+    }
+    if (n < MT_N)
+    {
+        entropy[n++] = clock();
+    }
 
     if (n < MT_N) {
         struct timeval tv;
@@ -173,11 +208,9 @@ int Random::range(int min, int max)
 
 double Random::ranged(double min, double max)
 {
-double rnd = (double)rand32() * (double)rand32();
+    double rnd = (double)rand32() * (double)rand32();
     double r = (max - min) + 1;
     double v = (rnd / ((double)maxrand() * (double)maxrand() + 1)) * r;
     return min + v;
 }
-
-
 
